27b9466028eab23ab901924e07a56c58dd94c705
Jason Estey <esteyj@yahoo.com>
2023-11-13 19:07:27 -0800

    first, ugly cut at shoot the moon logic

--- game/round.go
+++ game/round.go
@@ -15,6 +15,27 @@ func (round *Round) Scores() []int {
 		result[trick.Winner()] += trick.Score()
 	}
 
-	// TODO: check for "shoot the moon"
+	// check for "shoot the moon"
+	firstPlayerWithAnyPoints := -1
+	numberOfPlayersWithAnyPoints := 0
+	for i := range result {
+		if result[i] > 0 {
+			numberOfPlayersWithAnyPoints += 1
+			if firstPlayerWithAnyPoints == -1 {
+				firstPlayerWithAnyPoints = i
+			}
+		}
+	}
+	maxPoints := 26
+	if numberOfPlayersWithAnyPoints == 1 {
+		for i := range result {
+			if i == numberOfPlayersWithAnyPoints {
+				result[i] = 0
+			} else {
+				result[i] = maxPoints
+			}
+		}
+	}
+
 	return result
 }
